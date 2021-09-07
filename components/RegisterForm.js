dbc004f799c8c12c651b6e531c4f64998bf0b91e
jaanikaukonen <kaukonen.jaani@gmail.com>
2021-09-07 14:18:54 +0300

    Form validation

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -7,7 +7,7 @@ import useSignUpForm from '../hooks/RegisterHooks';
 import {useUser} from '../hooks/ApiHooks';
 
 const RegisterForm = ({navigation}) => {
-  const {inputs, registerErrors, handleInputChange, checkUsername} = useSignUpForm();
+  const {inputs, registerErrors, handleInputChange, handleOnEndEditing, checkUsername} = useSignUpForm();
   const {register} = useUser();
   const doRegister = async () => {
     const serverResponse = await register(
@@ -28,30 +28,53 @@ const RegisterForm = ({navigation}) => {
     <View style={{padding: 10}}>
       <FormTextInput
         autoCapitalize="none"
-        placeholder="username"
+        placeholder="Username"
         onChangeText={(txt) => handleInputChange('username', txt)}
         onEndEditing={(evt) => {
-          const availability = checkUsername(evt.nativeEvent.text);
+          handleOnEndEditing('username', evt.nativeEvent.text)
+          checkUsername(evt.nativeEvent.text);
         }}
         errorMessage={registerErrors.username}
       />
       <FormTextInput
         autoCapitalize="none"
-        placeholder="password"
+        placeholder="Password"
         onChangeText={(txt) => handleInputChange('password', txt)}
         secureTextEntry={true}
+        onEndEditing={(evt) => {
+          handleOnEndEditing('password', evt.nativeEvent.text)
+        }}
+        errorMessage={registerErrors.password}
       />
       <FormTextInput
         autoCapitalize="none"
-        placeholder="email"
+        placeholder="Confirm password"
+        onChangeText={(txt) => handleInputChange('confirm_password', txt)}
+        secureTextEntry={true}
+        onEndEditing={(evt) => {
+          handleOnEndEditing('confirm_password', evt.nativeEvent.text)
+        }}
+        errorMessage={registerErrors.confirm_password}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="Email"
         onChangeText={(txt) => handleInputChange('email', txt)}
+        onEndEditing={(evt) => {
+          handleOnEndEditing('email', evt.nativeEvent.text)
+        }}
+        errorMessage={registerErrors.email}
       />
       <FormTextInput
         autoCapitalize="none"
-        placeholder="full name"
+        placeholder="Full name (Optional)"
         onChangeText={(txt) => handleInputChange('full_name', txt)}
+        onEndEditing={(evt) => {
+          handleOnEndEditing('full_name', evt.nativeEvent.text)
+        }}
+        errorMessage={registerErrors.full_name}
       />
-      <Button raised title="Register" onPress={doRegister} />
+      <Button raised title="Register" onPress={doRegister} disabled={registerErrors.username || registerErrors.password || registerErrors.email} />
     </View>
   );
 };
--- hooks/RegisterHooks.js
+++ hooks/RegisterHooks.js
@@ -1,23 +1,79 @@
-import {useState} from 'react';
+import { useState } from "react";
 import { useUser } from "./ApiHooks";
+import { validator } from "../utils/validator";
+
+const constraints = {
+  username: {
+    presence: true,
+    length: {
+      minimum: 3,
+      message: "^Username must be at least 3 characters long"
+    }
+  },
+  password: {
+    presence: true,
+    length: {
+      minimum: 6,
+      message: "^Password must be at least 6 characters"
+    }
+  },
+  confirm_password: {
+    presence: true,
+    equality: {
+      attribute: "password",
+      message: "^Passwords doesn't match",
+    }
+
+  },
+  email: {
+    presence: true,
+    email: {
+      message: "^Enter a valid email address"
+    }
+  },
+  full_name: {
+    format: {
+      pattern: "[a-zA-Z]+",
+      message: "^Full name can only contain letters"
+    }
+  }
+
+};
 
 const useSignUpForm = (callback) => {
-  const {checkIfUsernameIsAvailable} = useUser();
+  const { checkIfUsernameIsAvailable } = useUser();
   const [inputs, setInputs] = useState({
-    username: '',
-    password: '',
-    email: '',
-    full_name: '',
+    username: "",
+    password: "",
+    email: "",
+    full_name: ""
   });
 
   const [registerErrors, setRegisterErrors] = useState({});
 
   const handleInputChange = (name, text) => {
-
     setInputs((inputs) => {
       return {
         ...inputs,
-        [name]: text,
+        [name]: text
+      };
+    });
+  };
+
+  const handleOnEndEditing = (name, text) => {
+
+    let error = validator(name, text, constraints);
+
+    if (name === 'confirm_password') {
+      error = validator(name, {password: inputs.password, confirm_password: text}, constraints)
+    } else {
+      error = validator(name, text, constraints);
+    }
+
+    setRegisterErrors((registerErrors) => {
+      return {
+        ...registerErrors,
+        [name]: error
       };
     });
   };
@@ -32,26 +88,22 @@ const useSignUpForm = (callback) => {
       const availability = await checkIfUsernameIsAvailable(username);
 
       if (!availability) {
-        setRegisterErrors( (registerErrors) => {
-          return {...registerErrors, username: 'Username already exists'};
-        });
-      } else {
-        setRegisterErrors( (registerErrors) => {
-          return {...registerErrors, username: null};
+        setRegisterErrors((registerErrors) => {
+          return { ...registerErrors, username: "Username already exists" };
         });
       }
-
     } catch (e) {
-      console.log('checkUsername failed' + e.message)
+      console.log("checkUsername failed" + e.message);
     }
   };
 
   return {
     handleInputChange,
+    handleOnEndEditing,
     inputs,
     registerErrors,
     setRegisterErrors,
-    checkUsername,
+    checkUsername
 
   };
 };
